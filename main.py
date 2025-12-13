a6b1c9fb9bbebb260c8101ecafcc5c4e45388bce
Winze88 <github@yooka.no>
2025-12-13 23:24:06 +0100

    added path to book as input

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import sys
 from stats import get_book_text, word_count, char_count, sort_dict
 
+try:
+    path = sys.argv[1]
+except Exception:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def main():
-    text = get_book_text("./books/frankenstein.txt")
+    text = get_book_text(path)
     count = word_count(text)
     char_count_result = char_count(text)
     sorted_dict = sort_dict(char_count_result)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}...")
     print("----------- Word Count ----------")
     print(f"Found {count} total words")
     print("--------- Character Count -------")
